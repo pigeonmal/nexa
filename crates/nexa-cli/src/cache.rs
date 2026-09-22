251616ca589546ff325f7f0e7d055d7b4704d9e5
pigeonmal <eliastafnakji1@gmail.com>
2026-09-22 12:08:02 +0200

    Invalidate cache after syntax cleanup

--- crates/nexa-cli/src/cache.rs
+++ crates/nexa-cli/src/cache.rs
@@ -13,7 +13,7 @@ use std::{
 
 use nexa_syntax::ast::Program;
 
-const CACHE_VERSION: &str = "build-v24";
+const CACHE_VERSION: &str = "build-v25";
 
 pub(super) struct CachedBuild {
     pub(super) warnings: Vec<String>,
